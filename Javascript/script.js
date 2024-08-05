57ae935c922ca14ea8872cc765f62c70d2aaecca
Jashwanth001 <jashwanthskt@gmail.com>
2024-08-05 20:19:52 +0530

    Updated filr

---
+++ Javascript/script.js
@@ -0,0 +1,86 @@
+// let price = 100;
+// let tax = 500;
+// let product = 'Apple';
+// let total = price + tax;
+// console.log(product)
+// console.log("The Price is "+total);
+
+//const price = document.getElementById("price");
+//document.write("The Price is "+price);
+
+// let Fruit_Name = "Mango";
+// let Count = 5;
+// let price = 70;
+// let total = Count*price;
+// console.log(Fruit_Name);
+// console.log("Total Price is "+total); 
+
+// function iphone(){
+//     console.log("Iphone 13 Pro Max is available");
+// }
+// iphone()
+
+// function redmi(){
+//     console.log("Redmi is available");
+// }
+// redmi()
+
+// let a = 10
+// let b = 10
+
+// function add(){
+//     console.log("Add:"+(a+b))
+// }
+// add()
+
+// let factor = "Vijay"
+// let fplayer = 'Rohit'
+// let fmovie = 'Theeran'
+// function favourite(){
+//     console.log("Favourite Actor:"+factor)
+//     console.log("Favourite player:"+fplayer)
+//     console.log("Favourite movie:"+fmovie)
+// }
+// favourite()
+
+// function area(length,breath){
+//     console.log(length*breath)
+// }
+
+// area(20,30)
+
+
+// let a = 10
+// let b = 10
+// function add(a,b){
+//     return a+b
+// }
+// var a = add(5,5)
+// console.log(a)
+
+// let raining = 'yes'
+// if(yes){
+//     console.log("Take an umbrella")
+// }
+// else{
+//     console.log("Enjoy the sunshine")
+// }
+
+let rain = true
+
+if(true){
+    console.log("Rainly")
+}
+else{
+    console.log("enjoy the rain")
+}
+
+
+let work = true
+
+if(true){
+    console.log("Great Job")
+}
+else{
+    console.log("Finish your work")
+}
